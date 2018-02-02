371c59b0e02e036eef42dc91ec254a65805eba35
Fabian <Fabian@DESKTOP-G8UQBJ7>
2018-02-02 01:22:46 -0600

    Pruebas

--- Proyect/src/proyect/Proyect.java
+++ Proyect/src/proyect/Proyect.java
@@ -16,7 +16,7 @@ public class Proyect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-                            
+                    System.out.println("HOLA MUNDO");        
     }
     
 }
