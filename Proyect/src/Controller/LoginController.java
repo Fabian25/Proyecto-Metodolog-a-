6840247dee83ebaac84c3a7a4ad44129aea9f3a3
AlonsoMesen14 <totomesen@hotmail.com>
2018-02-14 18:34:55 -0600

    Creacion interfaz login

--- Proyect/src/Controller/LoginController.java
+++ Proyect/src/Controller/LoginController.java
@@ -7,7 +7,14 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,6 +23,15 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
+    @FXML
+    private TextField txt_Usuario;
+    @FXML
+    private PasswordField txt_Contra;
+    @FXML
+    private Label lbl_Recuperar;
+    @FXML
+    private Button btn_Ingresar;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +39,13 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void RecuperarContra(MouseEvent event) {
+    }
+
+    @FXML
+    private void Ingresar(ActionEvent event) {
+    }
     
 }
--- Proyect/src/proyect/MainFX.java
+++ Proyect/src/proyect/MainFX.java
@@ -8,6 +8,8 @@ package proyect;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -21,24 +23,16 @@ public class MainFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+      try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/Login.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 
     /**
