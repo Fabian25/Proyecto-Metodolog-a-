6cb49a26eedec2b367badc7a9ed4900bebf141b7
AlonsoMesen14 <totomesen@hotmail.com>
2018-03-09 16:33:36 -0600

    Fix

--- Proyect/src/BaseDatos/Conexion.java
+++ Proyect/src/BaseDatos/Conexion.java
@@ -10,23 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-
-<<<<<<< HEAD
-            Connection con = null;
-    public static Connection conexion() {
-//
-//        try {
-//            conexion = null;
-//            Class.forName("com.mysql.jdbc.Driver");
-//             conexion = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/", "sql10218899", "dqlkLPTbHd");
-//            System.out.println("conexion establecida");
-//        } catch (ClassNotFoundException | SQLException e) {
-//            System.out.println("error de conexion");
-//            System.out.println("error de conexion " + e);
-//        }
-//        return conexion;
-//    }
-=======
+    
             Connection conexion = null;
     public  Connection conexion() {
 
@@ -40,7 +24,6 @@ public class Conexion {
             System.out.println("error de conexion " + e);
         }
         return conexion;
->>>>>>> 74a894483c9e37f03b7e72b8322bcc796af3c6b1
     }
 }
 
