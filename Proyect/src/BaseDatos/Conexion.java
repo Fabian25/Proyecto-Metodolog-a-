610d71a35f409d4c5cabb89ac8b41ab97856b780
jose zuniga <jsezuniga72@gmail.com>
2018-03-09 16:52:17 -0600

    conexion

--- Proyect/src/BaseDatos/Conexion.java
+++ Proyect/src/BaseDatos/Conexion.java
@@ -11,44 +11,20 @@ import java.sql.SQLException;
 import java.sql.*;
 import javax.swing.*;
 
-<<<<<<< HEAD
-public  class Conexion {
-
-            Connection con = null;
-
-public static   Connection conexion() {
-
-try
-{
-Class.forName("com.mysql.jdbc.Driver");
-Connection conn = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/","sql10218899","dqlkLPTbHd");
-return conn;
-}
-catch(Exception e)
-{
-
-return null;
-=======
 public class Conexion {
-    
-            Connection conexion = null;
-    public  Connection conexion() {
+
+    Connection con = null;
+
+    public static Connection conexion() {
 
         try {
-            conexion = null;
             Class.forName("com.mysql.jdbc.Driver");
-             conexion = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/", "sql10218899", "dqlkLPTbHd");
-            System.out.println("conexion establecida");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("error de conexion");
-            System.out.println("error de conexion " + e);
+            Connection conn = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/", "sql10218899", "dqlkLPTbHd");
+            return conn;
+        } catch (Exception e) {
+
+            return null;
+
         }
-        return conexion;
     }
->>>>>>> 6cb49a26eedec2b367badc7a9ed4900bebf141b7
 }
-}
-}
-
-
-
