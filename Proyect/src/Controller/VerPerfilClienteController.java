67648172272197da0560a0160c0b949c01712c2e
jose zuniga <jsezuniga72@gmail.com>
2018-04-06 16:22:31 -0600

    ID Cliente

--- Proyect/src/Controller/VerPerfilClienteController.java
+++ Proyect/src/Controller/VerPerfilClienteController.java
@@ -7,6 +7,11 @@ package Controller;
 
 import DAO.ClienteDAOImplements;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,6 +30,9 @@ import javafx.stage.StageStyle;
  * @author ALONSITO
  */
 public class VerPerfilClienteController implements Initializable {
+
+    PreparedStatement preparedStatement = null;
+    Connection connection = BaseDatos.Conexion.getConnection();
     ClienteDAOImplements h = new ClienteDAOImplements();
     @FXML
     private TextField txtCName;
@@ -49,14 +57,14 @@ public class VerPerfilClienteController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
-      private void CargarVistas(String Vista, String Titulo) {
+    private void CargarVistas(String Vista, String Titulo) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/" + Vista + ".fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
-             stage.initStyle(StageStyle.UNDECORATED);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.setTitle(Titulo);
             stage.setScene(new Scene(root1));
             stage.show();
@@ -66,7 +74,37 @@ public class VerPerfilClienteController implements Initializable {
             System.out.println("Error");
         }
     }
-      
+
+    private int Data() {
+
+        String sql = "SELECT Correo FROM sql10218899.UsuarioActual;";
+        String datos = " ";
+        int ce =0;
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs != null) {
+                while (rs.next()) {
+                    datos = rs.getString(2);
+                }
+            
+ String query = "{UsuarioActualClientes ActualizarCliente("+datos+")}";
+     Statement st2 = connection.createStatement();
+            ResultSet rs2 = st.executeQuery(sql);
+           if (rs != null) {
+             ce=Integer.parseInt(query);
+            }   
+            
+            }else{
+            
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return ce;
+    }
+
     @FXML
     private void C_BarMyProfile(ActionEvent event) {
         CargarVistas("VerPerfilCliente", "Profile");
@@ -79,7 +117,7 @@ public class VerPerfilClienteController implements Initializable {
 
     @FXML
     private void C_Home(ActionEvent event) {
-         CargarVistas("MenuCliente", "Profile");
+        CargarVistas("MenuCliente", "Profile");
     }
-    
+
 }
